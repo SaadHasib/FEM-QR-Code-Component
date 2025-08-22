4d7de24ed0c21cda32f56d752ad07b6af5b81902
Saad Hasib <saadhasib95@gmail.com>
2025-08-23 02:17:59 +0500

    Added TS path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, type AliasOptions } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+
+// @ts-expect-error missing types
+import path from "path";
+
+// @ts-expect-error missing types
+const root = path.resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": root,
+    } as AliasOptions,
+  },
 });
